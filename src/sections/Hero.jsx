a4960be92a9e41eab6c8efecb98947391a26ee36
will <williyanto658@gmail.com>
2025-05-12 23:09:22 +0700

    add animation hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { words } from "../constants/index.js";
 
 const Hero = () => {
-  return(
+  return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
         <img src="/images/bg.png" alt="background" />
@@ -10,17 +11,37 @@ const Hero = () => {
       <div className="hero-layout">
         {/* left side hero content */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-          <div className="flex flex-col gap-7">
+          <div className="flex flex-col gap-7 md:gap-100">
             <div className="hero-text">
-              <h1>Shaping Designs</h1>
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
               <h1>into real project</h1>
               <h1>that deliver result</h1>
             </div>
+             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">Hi, I'm will, a developer based in indonesia with passion for code.</p>
           </div>
         </header>
       </div>
     </section>
-  )
+  );
 };
 
 export default Hero;
